aa7479ca315cb9fb6863c59547728ddda0f71563
Alberto Valero Gómez <avalero@users.noreply.github.com>
2018-08-30 16:29:37 +0200

    Revert "added boolean operations to js"

--- src/lib/object3d/Intersection.js
+++ src/lib/object3d/Intersection.js
@@ -1,17 +1,15 @@
-import * as Three from 'three';
-
 import CompoundObject from './CompoundObject';
 import { ThreeBSP } from './threeCSG';
 
 export default class Intersection extends CompoundObject {
   getGeometry() {
     // First element of array
-    let intersectionMeshBSP = new ThreeBSP(this.children[0].getMess());
+    let intersectionMeshBSP = new ThreeBSP(this.children[0].getGeometry());
     // Intersect with the rest
     for (let i = 1; i < this.children.length; i += 1) {
-      const bspMesh = new ThreeBSP(this.children[i].getMesh());
+      const bspMesh = new ThreeBSP(this.children[i].getGeometry());
       intersectionMeshBSP = intersectionMeshBSP.intersect(bspMesh);
     }
-    return intersectionMeshBSP.toMesh(new Three.MeshLambertMaterial({color: 0xff0000}));
+    return intersectionMeshBSP.toMesh();
   }
 }
