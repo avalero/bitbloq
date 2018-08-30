487230f0af1c493d3e0ea7dc3884ec6580bcccc2
Alberto Valero <alberto.valero@bq.com>
2018-08-30 16:28:05 +0200

    added boolean operations to js

--- src/lib/object3d/Union.js
+++ src/lib/object3d/Union.js
@@ -1,16 +1,18 @@
+import * as Three from 'three';
+
 import CompoundObject from './CompoundObject';
 import { ThreeBSP } from './threeCSG';
 
 export default class Union extends CompoundObject {
   getGeometry() {
     // First element of array
-    let unionMeshBSP = new ThreeBSP(this.children[0].getGeometry());
+    let unionMeshBSP = new ThreeBSP(this.children[0].getMesh());
 
     // Union with the rest
     for (let i = 1; i < this.children.length; i += 1) {
-      const bspMesh = new ThreeBSP(this.children[i].getGeometry());
+      const bspMesh = new ThreeBSP(this.children[i].getMesh());
       unionMeshBSP = unionMeshBSP.union(bspMesh);
     }
-    return unionMeshBSP.toMesh();
+    return unionMeshBSP.toMesh(new Three.MeshLambertMaterial( {color: 0xff0000} ));
   }
 }
