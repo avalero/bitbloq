86696b3a3bc29f20560a70a59e2c5c425bab4103
Alberto Valero <alberto.valero@bq.com>
2018-09-04 12:46:53 +0200

    Added Rotation and Translation Helpers

---
+++ src/lib/object3d/TranslationHelper.js
@@ -0,0 +1,22 @@
+import * as Three from 'three';
+
+export default class TranslationHelper {
+
+  constructor(mesh, axis, relative = true) {
+
+    this.axesHelper = new Three.AxesHelper(20);
+
+    this.axesHelper.position.x = mesh.position.x;
+    this.axesHelper.position.y = mesh.position.y;
+    this.axesHelper.position.z = mesh.position.z;
+
+
+    if (relative) {
+      this.axesHelper.setRotationFromEuler(this.mesh.rotation);
+    }
+  }
+
+  get mesh() {
+    return this.axesHelper;
+  }
+}
