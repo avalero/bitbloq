c9693f65a2bd77ec41dd97cfce5d44484e69fc56
Alberto Valero <alberto.valero@bq.com>
2018-09-11 23:22:01 +0200

    translation helpers working

--- src/lib/object3d/TranslationHelper.js
+++ src/lib/object3d/TranslationHelper.js
@@ -3,33 +3,38 @@ import { ThreeBSP } from './threeCSG';
 
 export default class TranslationHelper {
   constructor(mesh, axis, relative) {
-    console.log(`Translate: ${axis}`);
-
     const bbox = new Three.Box3().setFromObject(mesh);
     const radius = 0.5;
-    let length;
     let color;
     const separation = 3;
-    const inner_lenght = 20;
+    const length = 20;
+    const arrowLength = 5;
+    const boundingBoxDims = new Three.Vector3();
+    bbox.getSize(boundingBoxDims);
+    let offset;
+    let offsetArrow;
 
     if (axis === 'x') {
       color = 0xff0000;
-      length = bbox.getSize().x + inner_lenght;
+      offset = boundingBoxDims.x / 2 + separation + length / 2;
+      offsetArrow = boundingBoxDims.x / 2 + separation + arrowLength / 2 + length;
     } else if (axis === 'y') {
       color = 0x0000ff;
-      length = bbox.getSize().y + inner_lenght;
+      offset = boundingBoxDims.y / 2 + separation + length / 2;
+      offsetArrow = boundingBoxDims.y / 2 + separation + arrowLength / 2 + length;
     } else {
       color = 0x00ff00;
-      length = bbox.getSize().z + inner_lenght;
+      offset = boundingBoxDims.z / 2 + separation + length / 2;
+      offsetArrow = boundingBoxDims.z / 2 + separation + arrowLength / 2 + length;
     }
 
     const cylinderGeometry = new Three.CylinderGeometry(radius, radius, length);
     cylinderGeometry.rotateZ(Math.PI / 2);
-    cylinderGeometry.translate( (length  + inner_lenght) / 2 + separation, 0, 0);
+    cylinderGeometry.translate(offset, 0, 0);
 
     const arrowGeometry = new Three.CylinderGeometry(2 * radius, 0, 5, 20);
     arrowGeometry.rotateZ(Math.PI / 2);
-    arrowGeometry.translate((length  + inner_lenght) / 2 + separation, 0, 0);
+    arrowGeometry.translate(offsetArrow, 0, 0);
 
     let cylinderBSPGeometry = new ThreeBSP(cylinderGeometry);
     const arrowBSPGeometry = new ThreeBSP(arrowGeometry);
