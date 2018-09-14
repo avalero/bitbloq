160406c5515e619cc51d2731036e8818377307a6
Alberto Valero <alberto.valero@bq.com>
2018-09-14 11:00:11 +0200

    fixed error

--- src/lib/object3d/STLObject.js
+++ src/lib/object3d/STLObject.js
@@ -10,16 +10,12 @@
  * @author Alberto Valero <https://github.com/avalero>
  *
  * Created at     : 2018-09-14 10:48:00 
- * Last modified  : 2018-09-14 10:52:15
+ * Last modified  : 2018-09-14 10:59:53
  */
 
 
 
 import Object3D from './Object3D';
-import * as Three from 'three';
-var STLLoader = require('three-stl-loader')(Three)
-
-var loader = new STLLoader()
 
 export default class STLObject extends Object3D {
 
