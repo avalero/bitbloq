c7ecdce8a38daf1e640db56477d833a809c6039c
Alberto Valero <alberto.valero@bq.com>
2018-12-19 09:29:02 +0100

    setting tests for lib3d

---
+++ packages/lib3d/src/__tests__/Scene.test.ts
@@ -0,0 +1,67 @@
+import "jsdom-worker";
+import Scene from "../Scene";
+import Cube from "../Cube";
+
+test("Scene - Constructor", () => {
+  const scene = new Scene();
+  expect((scene as any).objectCollector).toEqual([]);
+  expect((scene as any).objectsInScene).toEqual([]);
+  expect((scene as any).objectsInTransition).toEqual([]);
+  expect((scene as any).history).toEqual([]);
+  expect((scene as any).historyIndex).toEqual(-1);
+});
+
+test("Scene - Can Redo/Undo", () => {
+  const scene = new Scene();
+  expect(scene.canRedo()).toEqual(false);
+  expect(scene.canUndo()).toEqual(false);
+
+  const cube = new Cube({ width: 10, height: 10, depth: 10 });
+  scene.addNewObjectFromJSON(cube.toJSON());
+
+  expect(scene.canRedo()).toEqual(false);
+  expect(scene.canUndo()).toEqual(true);
+
+  scene.addNewObjectFromJSON(cube.toJSON());
+
+  (scene as any).historyIndex = 0;
+
+  expect(scene.canRedo()).toEqual(true);
+  expect(scene.canUndo()).toEqual(true);
+});
+
+test("Scene - Undo - 1 step", () => {
+  const scene = new Scene();
+  const firstJSON = scene.toJSON();
+
+  const cube = new Cube({ width: 10, height: 10, depth: 10 });
+  scene.addNewObjectFromJSON(cube.toJSON());
+
+  scene.undo();
+  const lastJSON = scene.toJSON();
+
+  expect(firstJSON).toEqual(lastJSON);
+});
+
+test("Scene - Undo - 2 steps", () => {
+  const scene = new Scene();
+  const firstJSON = scene.toJSON();
+
+  const cube = new Cube({ width: 10, height: 10, depth: 10 });
+
+  scene.addNewObjectFromJSON(cube.toJSON());
+  const secondJSON = scene.toJSON();
+
+  scene.addNewObjectFromJSON(cube.toJSON());
+
+  scene.undo();
+
+  expect(scene.toJSON()).toEqual(secondJSON);
+
+  scene.undo();
+
+  expect(scene.toJSON()).toEqual(firstJSON);
+
+  const errorfn = () => scene.undo();
+  expect(errorfn).toThrowError();
+});
