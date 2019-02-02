a6216f19fbf81913856668f25ddcfe84e57e52b6
Alberto Valero <alberto.valero@bq.com>
2019-02-02 13:10:18 +0100

    parsing stl to json

--- packages/stl2bitbloqjson/src/index.ts
+++ packages/stl2bitbloqjson/src/index.ts
@@ -5,26 +5,27 @@ import * as fs from 'fs';
 
 interface ISTLParams {
   blob: {
-    buffer: ArrayBuffer;
+    uint8Data: Uint8Array;
     filetype: string;
+    newfile: boolean;
   };
 }
 
-const stlFolder = './stl/';
-const jsonFolder = './json/';
+const stlFolder = './stl';
+const jsonFolder = './json';
 
 fs.readdirSync(stlFolder).forEach(async file => {
   console.log(`Converting ${file} ...`);
 
   try {
     const blob: ArrayBuffer = fs.readFileSync(`${stlFolder}/${file}`);
-
-    // const buffer: ArrayBuffer = new Uint8Array(blob).buffer;
+    const uint8Data: Uint8Array = new Uint8Array(blob);
 
     const parameters: ISTLParams = {
       blob: {
-        buffer: blob,
+        uint8Data,
         filetype: 'model/x.stl-binary',
+        newfile: true,
       },
     };
 
@@ -34,7 +35,9 @@ fs.readdirSync(stlFolder).forEach(async file => {
     json.id = '';
     delete json.viewOptions;
     const jsonFileName: string = `${file.substr(0, file.length - 3)}json`;
+    console.log(`${jsonFolder}/${jsonFileName}`);
     fs.writeFileSync(`${jsonFolder}/${jsonFileName}`, JSON.stringify(json));
+    console.log(JSON.stringify(json));
     console.log('Done!');
   } catch (e) {
     console.error(e);
