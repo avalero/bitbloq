3d066e3e0f0d3e9a2e32989e8a00ec2868ea2e56
David Garcia Paredes <david.garciaparedes@bq.com>
2019-02-13 12:34:23 +0100

    Change project title by clicking on title

--- src/components/ThreeDEditor.tsx
+++ src/components/ThreeDEditor.tsx
@@ -1,13 +1,14 @@
 import * as React from "react";
 import styled from "@emotion/styled";
-import saveAs from 'file-saver';
+import saveAs from "file-saver";
 import { Query, Mutation } from "react-apollo";
 import debounce from "lodash.debounce";
 import { ThreeD } from "@bitbloq/3d";
 import { colors, Document, Icon, Spinner, withTranslate } from "@bitbloq/ui";
 import gql from "graphql-tag";
 import DocumentInfoForm from "./DocumentInfoForm";
-import {addShapeGroups} from "../config";
+import EditTitleModal from "./EditTitleModal";
+import { addShapeGroups } from "../config";
 
 const DOCUMENT_QUERY = gql`
   query Document($id: ObjectID!) {
@@ -33,9 +34,9 @@ const UPDATE_DOCUMENT_MUTATION = gql`
     updateDocument(
       id: $id
       input: {
-        title: $title,
-        content: $content,
-        description: $description,
+        title: $title
+        content: $content
+        description: $description
         image: $image
       }
     ) {
@@ -47,38 +48,58 @@ const UPDATE_DOCUMENT_MUTATION = gql`
   }
 `;
 
-const getMenuOptions = (baseMenuOptions, t) => (
-  [
-    {
-      id: 'file',
-      label: t('menu-file'),
-      children: [
-        {
-          id: 'download-document',
-          label: t('menu-download-document'),
-          icon: <Icon name="download" />
-        },
-        {
-          id: 'download-stl',
-          label: t('menu-download-stl'),
-          icon: <Icon name="threed" />
-        },
-      ],
-    },
-    ...baseMenuOptions
-  ]
-);
+const getMenuOptions = (baseMenuOptions, t) => [
+  {
+    id: "file",
+    label: t("menu-file"),
+    children: [
+      {
+        id: "download-document",
+        label: t("menu-download-document"),
+        icon: <Icon name="download" />
+      },
+      {
+        id: "download-stl",
+        label: t("menu-download-stl"),
+        icon: <Icon name="threed" />
+      }
+    ]
+  },
+  ...baseMenuOptions
+];
 
-class ThreeDEditor extends React.Component {
+class State {
+  readonly isEditTitleVisible: boolean = false;
+}
+
+class ThreeDEditor extends React.Component<any, State> {
+  readonly state = new State();
   private currentContent: any;
   threedRef = React.createRef<ThreeD>();
 
-  onEditTitle(document) {}
+  onEditTitle(document) {
+    this.setState({ isEditTitleVisible: true });
+  }
+
+  onSaveTitle = (title, document) => {
+    const { updateDocument } = this.props;
+    const { id, description, content, image } = document;
+    updateDocument({
+      variables: { id, title, description, content, image },
+      refetchQueries: [
+        {
+          query: DOCUMENT_QUERY,
+          variables: { id }
+        }
+      ]
+    });
+    this.setState({ isEditTitleVisible: false });
+  };
 
   onMenuOptionClick = (option, document) => {
     const { type, title, description, content, image } = document;
     switch (option.id) {
-      case 'download-document':
+      case "download-document":
         const documentJSON = {
           type,
           title,
@@ -86,22 +107,20 @@ class ThreeDEditor extends React.Component {
           content,
           image
         };
-        var blob = new Blob(
-          [JSON.stringify(documentJSON)],
-          {type: "text/json;charset=utf-8"}
-        )
+        var blob = new Blob([JSON.stringify(documentJSON)], {
+          type: "text/json;charset=utf-8"
+        });
         saveAs(blob, `${title}.3d.bitbloq`);
         return;
 
-      case 'download-stl':
+      case "download-stl":
         this.threedRef.current.exportToSTL();
         return;
 
       default:
-        onMenuOptionClick && onMenuOptionClick(option)
         return;
     }
-  }
+  };
 
   renderInfoTab(document) {
     const { t, updateDocument } = this.props;
@@ -134,52 +153,70 @@ class ThreeDEditor extends React.Component {
   }
 
   render() {
+    const { isEditTitleVisible } = this.state;
     const { id, updateDocument, t } = this.props;
 
     return (
-      <Query query={DOCUMENT_QUERY} variables={{ id }}>
-        {({ loading, error, data }) => {
-          if (loading) return <Loading />;
-          if (error) return <p>Error :(</p>;
+      <>
+        <Query query={DOCUMENT_QUERY} variables={{ id }}>
+          {({ loading, error, data }) => {
+            if (loading) return <Loading />;
+            if (error) return <p>Error :(</p>;
 
-          const { document } = data;
-          const { id, title } = document;
-          let content = [];
-          try {
-            content = JSON.parse(document.content);
-          } catch (e) {
-            console.warn("Error parsing document content", e);
-          }
+            const { document } = data;
+            const { id, title } = document;
+            let content = [];
+            try {
+              content = JSON.parse(document.content);
+            } catch (e) {
+              console.warn("Error parsing document content", e);
+            }
 
-          this.currentContent = content;
+            this.currentContent = content;
 
-          return (
-            <ThreeD
-              ref={this.threedRef}
-              initialContent={content}
-              title={title}
-              canEditTitle
-              onEditTitle={() => this.onEditTitle(document)}
-              menuOptions={base => getMenuOptions(base, t)}
-              addShapeGroups={base => ([...base, ...addShapeGroups])}
-              onMenuOptionClick={option => this.onMenuOptionClick(option, document)}
-              onContentChange={content => {
-                this.currentContent = content;
-                updateDocument({
-                  variables: {
-                    id,
-                    title,
-                    content: JSON.stringify(content)
-                  },
-                  refetchQueries: [{ query: DOCUMENT_QUERY, variables: { id } }]
-                });
-              }}
-            >
-              {mainTab => [mainTab, this.renderInfoTab(document)]}
-            </ThreeD>
-          );
-        }}
-      </Query>
+            return (
+              <>
+                <ThreeD
+                  ref={this.threedRef}
+                  initialContent={content}
+                  title={title}
+                  canEditTitle
+                  onEditTitle={() => this.onEditTitle(document)}
+                  menuOptions={base => getMenuOptions(base, t)}
+                  addShapeGroups={base => [...base, ...addShapeGroups]}
+                  onMenuOptionClick={option =>
+                    this.onMenuOptionClick(option, document)
+                  }
+                  onContentChange={content => {
+                    this.currentContent = content;
+                    updateDocument({
+                      variables: {
+                        id,
+                        title,
+                        content: JSON.stringify(content)
+                      },
+                      refetchQueries: [
+                        { query: DOCUMENT_QUERY, variables: { id } }
+                      ]
+                    });
+                  }}
+                >
+                  {mainTab => [mainTab, this.renderInfoTab(document)]}
+                </ThreeD>
+                {isEditTitleVisible && (
+                  <EditTitleModal
+                    title={title}
+                    onCancel={() =>
+                      this.setState({ isEditTitleVisible: false })
+                    }
+                    onSave={title => this.onSaveTitle(title, document)}
+                  />
+                )}
+              </>
+            );
+          }}
+        </Query>
+      </>
     );
   }
 }
