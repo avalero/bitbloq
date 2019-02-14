1fb71d5e1fa85f684dac16efef60e5f517a7dc91
Paco Álvaro <33430955+fraalvper@users.noreply.github.com>
2019-02-14 17:01:48 +0100

    path for env updated

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh -xe
 if [ -d /root/config/ ]; then
-    #cp /root/config/env /usr/src/app/app/res/config/.env
-    ls -l /root/config/
+    cp /root/config/env /usr/src/app/api/.env
 fi
 npm start
