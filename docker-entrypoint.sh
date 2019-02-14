25ce8fb6eba3d27846d83ce7587ee1b21b02041d
Paco Álvaro <33430955+fraalvper@users.noreply.github.com>
2019-02-14 16:42:44 +0100

    .

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -xe
-#if [ -d /root/config/ ]; then
-#    cp /root/config/env /usr/src/app/app/res/config/.env
-#fi
+if [ -d /root/config/ ]; then
+    #cp /root/config/env /usr/src/app/app/res/config/.env
+    ls -l /root/config/
+fi
 npm start
