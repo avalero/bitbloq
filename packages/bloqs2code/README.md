9d920096b07d1ca3fc9ecf59da0c5cbfc28dc309
Alberto Valero <alberto.valero@bq.com>
2019-03-07 15:25:06 +0100

    new bloqs2code library

---
+++ packages/bloqs2code/README.md
@@ -0,0 +1,11 @@
+# `bloqs2code`
+
+> TODO: description
+
+## Usage
+
+```
+const bloqs2code = require('bloqs2code');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/bloqs2code/src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Root = () => <h1>Bloqs</h1>;
+
+ReactDOM.render(
+  <Root />,
+  document.getElementById("index")
+);
