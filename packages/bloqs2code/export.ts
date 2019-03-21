37dfd21b689a5b9aa5e79956926e0f719e604b3d
Alberto Valero <alberto.valero@bq.com>
2019-03-21 14:11:23 +0100

    removed former library

--- packages/bloqs2code/export.ts
+++
@@ -1,3 +0,0 @@
-import Bloqs2Code from './src/bloqs2code';
-
-export { Bloqs2Code };
--- packages/bloqs2code/src/interfaces.ts
+++
@@ -1,9 +0,0 @@
-export interface IBloqsJSON {
-  id: string;
-  type: string;
-}
-
-export interface IDigitalWriteJSON extends IBloqsJSON {
-  value: boolean;
-  pin: number;
-}
