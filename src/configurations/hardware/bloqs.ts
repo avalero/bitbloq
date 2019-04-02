96b0aa3e7bf5854fe8e8dcd3ef8cac4141950623
Alberto Valero <alberto.valero@bq.com>
2019-04-02 18:36:38 +0200

    configurations dir

---
+++ src/configurations/hardware/bloqs.ts
@@ -0,0 +1,14 @@
+const juniorbloqs = [
+  {
+    category: "action",
+    name: "TurnLedOn",
+    icon: "doubleLedOffOffIcon",
+    parameterDefinitions: [{
+      name: "led",
+      label: "bloq-parameter-led",
+      type: "selectComponent",
+      componentType: "DoubleLed"
+    }],
+    code: {}
+  }
+];
