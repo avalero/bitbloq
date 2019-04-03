3e29f13c46fa8c62992ee90a55b887ec77c477ef
Alberto Valero <alberto.valero@bq.com>
2019-04-03 21:12:49 +0200

    added components file for tests

--- packages/bloqs/src/bloqs2code/__tests__/config/boards.ts
+++ packages/bloqs/src/bloqs2code/__tests__/config/boards.ts
@@ -1,173 +1,173 @@
-import { IBoard, IPortDirection } from "../../../index";
+import { IBoard, IPortDirection } from '../../../index';
 
 export const boards: IBoard[] = [
   {
-    name: "zumjunior",
+    name: 'zumjunior',
     code: {
-      includes: ["<BQZUMJunior.h>", "<BQZUMJuniorPorts.h>"],
-      globals: ["BQ::ZUMJunior zumJunior;"],
-      setup: ["zumJunior.setup();"]
+      includes: ['<BQZUMJunior.h>', '<BQZUMJuniorPorts.h>'],
+      globals: ['BQ::ZUMJunior zumJunior;'],
+      setup: ['zumJunior.setup();'],
     },
     image: {
-      url: "https://bitbloq.bq.com/images/boards/1548099714577.zumjunior.svg",
+      url: 'https://bitbloq.bq.com/images/boards/1548099714577.zumjunior.svg',
       width: 300,
-      height: 300
+      height: 300,
     },
     ports: [
       {
-        name: "1",
+        name: '1',
         position: {
           x: -0.9,
-          y: 0.15
+          y: 0.15,
         },
-        connectorTypes: ["zumjunior-digital", "zumjunior-analog"],
+        connectorTypes: ['zumjunior-digital', 'zumjunior-analog'],
         pins: [
           {
-            name: "0",
-            value: "BQ::ZUMJunior::ports[1][0]"
+            name: '0',
+            value: 'BQ::ZUMJunior::ports[1][0]',
           },
           {
-            name: "1",
-            value: "BQ::ZUMJunior::ports[1][1]"
-          }
+            name: '1',
+            value: 'BQ::ZUMJunior::ports[1][1]',
+          },
         ],
         placeholderPosition: {
           x: -1.5,
-          y: 0.8
+          y: 0.8,
         },
-        direction: IPortDirection.West
+        direction: IPortDirection.West,
       },
       {
-        name: "2",
+        name: '2',
         position: {
           x: -0.9,
-          y: -0.15
+          y: -0.15,
         },
-        connectorTypes: ["zumjunior-digital", "zumjunior-analog"],
+        connectorTypes: ['zumjunior-digital', 'zumjunior-analog'],
         pins: [
           {
-            name: "0",
-            value: "BQ::ZUMJunior::ports[2][0]"
+            name: '0',
+            value: 'BQ::ZUMJunior::ports[2][0]',
           },
           {
-            name: "1",
-            value: "BQ::ZUMJunior::ports[2][1]"
-          }
+            name: '1',
+            value: 'BQ::ZUMJunior::ports[2][1]',
+          },
         ],
         placeholderPosition: {
           x: -1.5,
-          y: -0.8
+          y: -0.8,
         },
-        direction: IPortDirection.West
+        direction: IPortDirection.West,
       },
       {
-        name: "3",
+        name: '3',
         position: {
           x: 0.9,
-          y: -0.15
+          y: -0.15,
         },
-        connectorTypes: ["zumjunior-digital", "zumjunior-analog"],
+        connectorTypes: ['zumjunior-digital', 'zumjunior-analog'],
         pins: [
           {
-            name: "0",
-            value: "BQ::ZUMJunior::ports[3][0]"
+            name: '0',
+            value: 'BQ::ZUMJunior::ports[3][0]',
           },
           {
-            name: "1",
-            value: "BQ::ZUMJunior::ports[3][1]"
-          }
+            name: '1',
+            value: 'BQ::ZUMJunior::ports[3][1]',
+          },
         ],
         placeholderPosition: {
           x: 1.5,
-          y: -0.8
+          y: -0.8,
         },
-        direction: IPortDirection.East
+        direction: IPortDirection.East,
       },
       {
-        name: "4",
+        name: '4',
         position: {
           x: 0.9,
-          y: 0.15
+          y: 0.15,
         },
-        connectorTypes: ["zumjunior-digital", "zumjunior-analog"],
+        connectorTypes: ['zumjunior-digital', 'zumjunior-analog'],
         pins: [
           {
-            name: "0",
-            value: "BQ::ZUMJunior::ports[4][0]"
+            name: '0',
+            value: 'BQ::ZUMJunior::ports[4][0]',
           },
           {
-            name: "1",
-            value: "BQ::ZUMJunior::ports[4][1]"
-          }
+            name: '1',
+            value: 'BQ::ZUMJunior::ports[4][1]',
+          },
         ],
         placeholderPosition: {
           x: 1.5,
-          y: 0.8
+          y: 0.8,
         },
-        direction: IPortDirection.East
+        direction: IPortDirection.East,
       },
       {
-        name: "A",
+        name: 'A',
         position: {
           x: 0.15,
-          y: 0.9
+          y: 0.9,
         },
         connectorTypes: [
-          "zumjunior-digital",
-          "zumjunior-analog",
-          "zumjunior-i2c"
+          'zumjunior-digital',
+          'zumjunior-analog',
+          'zumjunior-i2c',
         ],
         pins: [
           {
-            name: "0",
-            value: "BQ::ZUMJunior::ports[A][0]"
+            name: '0',
+            value: 'BQ::ZUMJunior::ports[A][0]',
           },
           {
-            name: "1",
-            value: "BQ::ZUMJunior::ports[A][0]"
+            name: '1',
+            value: 'BQ::ZUMJunior::ports[A][1]',
           },
           {
-            name: "i2c",
-            value: "BQ::ZUMJunior::i2cPorts[A]"
-          }
+            name: 'i2c',
+            value: 'BQ::ZUMJunior::i2cPorts[A]',
+          },
         ],
         placeholderPosition: {
           x: 0.8,
-          y: 1.5
+          y: 1.5,
         },
-        direction: IPortDirection.North
+        direction: IPortDirection.North,
       },
       {
-        name: "B",
+        name: 'B',
         position: {
           x: -0.15,
-          y: 0.9
+          y: 0.9,
         },
         connectorTypes: [
-          "zumjunior-digital",
-          "zumjunior-analog",
-          "zumjunior-i2c"
+          'zumjunior-digital',
+          'zumjunior-analog',
+          'zumjunior-i2c',
         ],
         pins: [
           {
-            name: "0",
-            value: "BQ::ZUMJunior::ports[B][0]"
+            name: '0',
+            value: 'BQ::ZUMJunior::ports[B][0]',
           },
           {
-            name: "1",
-            value: "BQ::ZUMJunior::ports[B][0]"
+            name: '1',
+            value: 'BQ::ZUMJunior::ports[B][1]',
           },
           {
-            name: "i2c",
-            value: "BQ::ZUMJunior::i2cPorts[B]"
-          }
+            name: 'i2c',
+            value: 'BQ::ZUMJunior::i2cPorts[B]',
+          },
         ],
         placeholderPosition: {
           x: -0.8,
-          y: 1.5
+          y: 1.5,
         },
-        direction: IPortDirection.North
-      }
-    ]
-  }
+        direction: IPortDirection.North,
+      },
+    ],
+  },
 ];
