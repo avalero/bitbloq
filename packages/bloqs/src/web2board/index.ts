1898050db1509335e3fac3e6a301c66f144c5ae0
Alberto Valero <alberto.valero@bq.com>
2019-04-16 11:41:13 +0200

    web2board tries to self open

--- packages/bloqs/src/web2board/index.ts
+++ packages/bloqs/src/web2board/index.ts
@@ -30,31 +30,55 @@ export default class Web2Board {
   }
 
   public startWeb2board() {
-    console.warn("starting Web2board...");
-    const tempA = document.createElement("a");
-    tempA.setAttribute("href", "qssweb2board://");
+    const tempA = document.createElement('a');
+    tempA.setAttribute('href', 'qssweb2board://');
     document.body.appendChild(tempA);
     tempA.click();
     document.body.removeChild(tempA);
   }
 
-  public waitUntilOpened() {
+  public connect() {
     return new Promise((resolve, reject) => {
       if (!this.ws) {
         this.ws = new WebSocket(this.url);
       }
+
       if (this.ws.readyState === WebSocket.OPEN) {
         resolve();
+      } else if (this.ws.readyState === WebSocket.CLOSED) {
+        this.ws = null;
+        const error = new Error();
+        error.name = 'ConnectionError';
+        reject(error);
       } else {
-        this.ws.addEventListener("open", () => resolve());
-        this.ws.addEventListener("close", e => {
+        this.ws.addEventListener('open', () => resolve());
+        this.ws.addEventListener('close', e => {
           this.ws = null;
           const error = new Error();
-          error.name = "ConnectionError";
+          error.name = 'ConnectionError';
           reject(error);
         });
       }
     });
+  }
+
+  public async waitUntilOpened() {
+    let tries = 0;
+    while (tries < 10) {
+      console.info(`Connecting to Web2Board: ${tries} out of 10`);
+      try {
+        await this.connect();
+        return;
+      } catch (e) {
+        if (tries === 0) {
+          this.startWeb2board();
+        }
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        tries += 1;
+      }
+    }
+    this.ws = null;
+    throw new Error();
   }
 
   public waitForReply(id: number): Promise<IWeb2BoardResponse> {
@@ -63,13 +87,13 @@ export default class Web2Board {
         const reply = JSON.parse(event.data);
         if (reply.ID === id) {
           if (this.ws) {
-            this.ws.removeEventListener("message", onMessage);
+            this.ws.removeEventListener('message', onMessage);
           }
           resolve(reply);
         }
       };
       if (this.ws) {
-        this.ws.addEventListener("message", onMessage);
+        this.ws.addEventListener('message', onMessage);
       }
     });
   }
@@ -84,7 +108,7 @@ export default class Web2Board {
     const command = {
       args,
       ID: id,
-      function: fn
+      function: fn,
     };
     if (this.ws) {
       this.ws.send(JSON.stringify(command));
@@ -92,16 +116,16 @@ export default class Web2Board {
     return this.waitForReply(id);
   }
 
-  public getVersion = () => this.sendCommand("get_version", []);
+  public getVersion = () => this.sendCommand('get_version', []);
 
-  public setLibVersion = () => this.sendCommand("set_lib_version", ["1.6.0"]);
+  public setLibVersion = () => this.sendCommand('set_lib_version', ['1.6.0']);
 
   public compile(code: string, board: string) {
-    return this.callGenerator("compile", [code, board]);
+    return this.callGenerator('compile', [code, board]);
   }
 
   public upload(code: string, board: string) {
-    return this.callGenerator("upload", [code, board]);
+    return this.callGenerator('upload', [code, board]);
   }
 
   private async *callGenerator(fn: string, args: string[] = []) {
@@ -113,17 +137,17 @@ export default class Web2Board {
           return response;
         }
 
-        if (reply.title === "BOARDNOTDETECTED") {
+        if (reply.title === 'BOARDNOTDETECTED') {
           const error = new Error();
-          error.name = "BoardNotDetected";
+          error.name = 'BoardNotDetected';
           throw error;
-        } else if (reply.title === "BOARDNOTKNOWN") {
+        } else if (reply.title === 'BOARDNOTKNOWN') {
           const error = new Error();
-          error.name = "BoardNotKnown";
+          error.name = 'BoardNotKnown';
           throw error;
         } else if (reply.file) {
           const error = new Error(reply.error);
-          error.name = "CompileError";
+          error.name = 'CompileError';
           throw error;
         }
       }
