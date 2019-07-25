760870c17e1fbd89ea4844faa573ff111eec0b24
Alberto Valero <alberto.valero@bq.com>
2019-07-25 13:49:43 +0200

    groups have rotation and translation helper on each object

--- packages/lib3d/src/RotationHelper.ts
+++ packages/lib3d/src/RotationHelper.ts
@@ -13,76 +13,103 @@
  * Last modified  : 2018-12-16 19:55:16
  */
 
-import * as THREE from "three";
+import * as THREE from 'three';
 
 export default class RotationHelper {
   private helperMesh: THREE.Group;
 
   constructor(mesh: THREE.Object3D, axis: string, relative: boolean) {
-    const boundingBoxDims: THREE.Vector3 = new THREE.Vector3();
-    new THREE.Box3().setFromObject(mesh).getSize(boundingBoxDims);
-    const radius: number = 0.5;
-    let color: number;
-    const separation: number = 10;
-    let length: number;
-    const extraLength: number = 30;
-    let toroidRadius: number;
-    const toroidArc: number = 2 * Math.PI;
-    const toroidInnerRadius: number = 0.7;
+    if (
+      mesh instanceof THREE.Mesh ||
+      (mesh.userData &&
+        mesh.userData.type &&
+        mesh.userData.type === 'RepetitionObject')
+    ) {
+      const boundingBoxDims: THREE.Vector3 = new THREE.Vector3();
+      new THREE.Box3().setFromObject(mesh).getSize(boundingBoxDims);
+      const radius: number = 0.5;
+      let color: number;
+      const separation: number = 10;
+      let length: number;
+      const extraLength: number = 30;
+      let toroidRadius: number;
+      const toroidArc: number = 2 * Math.PI;
+      const toroidInnerRadius: number = 0.7;
 
-    if (axis === "x") {
-      color = 0xff0000;
-      toroidRadius =
-        Math.max(boundingBoxDims.y, boundingBoxDims.z) / 2 + separation;
-      length = Math.max(boundingBoxDims.y, boundingBoxDims.z) + extraLength;
-    } else if (axis === "y") {
-      color = 0x00ff00;
-      toroidRadius =
-        Math.max(boundingBoxDims.x, boundingBoxDims.z) / 2 + separation;
-      length = Math.max(boundingBoxDims.x, boundingBoxDims.z) + extraLength;
-    } else {
-      color = 0x0000ff;
-      toroidRadius =
-        Math.max(boundingBoxDims.x, boundingBoxDims.y) / 2 + separation;
-      length = Math.max(boundingBoxDims.x, boundingBoxDims.y) + extraLength;
-    }
+      if (axis === 'x') {
+        color = 0xff0000;
+        toroidRadius =
+          Math.max(boundingBoxDims.y, boundingBoxDims.z) / 2 + separation;
+        length = Math.max(boundingBoxDims.y, boundingBoxDims.z) + extraLength;
+      } else if (axis === 'y') {
+        color = 0x00ff00;
+        toroidRadius =
+          Math.max(boundingBoxDims.x, boundingBoxDims.z) / 2 + separation;
+        length = Math.max(boundingBoxDims.x, boundingBoxDims.z) + extraLength;
+      } else {
+        color = 0x0000ff;
+        toroidRadius =
+          Math.max(boundingBoxDims.x, boundingBoxDims.y) / 2 + separation;
+        length = Math.max(boundingBoxDims.x, boundingBoxDims.y) + extraLength;
+      }
 
-    const cylinderGeometry = new THREE.CylinderGeometry(radius, radius, length);
-    cylinderGeometry.rotateZ(Math.PI / 2);
+      const cylinderGeometry = new THREE.CylinderGeometry(
+        radius,
+        radius,
+        length
+      );
+      cylinderGeometry.rotateZ(Math.PI / 2);
 
-    const toroidGeometry = new THREE.TorusGeometry(
-      toroidRadius,
-      toroidInnerRadius,
-      6,
-      toroidRadius * 2, // lets make number of segments equal to radius * factor
-      toroidArc
-    );
+      const toroidGeometry = new THREE.TorusGeometry(
+        toroidRadius,
+        toroidInnerRadius,
+        6,
+        toroidRadius * 2, // lets make number of segments equal to radius * factor
+        toroidArc
+      );
 
-    toroidGeometry.rotateY(Math.PI / 2);
+      toroidGeometry.rotateY(Math.PI / 2);
 
-    const material = new THREE.MeshBasicMaterial({
-      color,
-      opacity: 0.5,
-      transparent: true,
-      depthWrite: false
-    });
+      const material = new THREE.MeshBasicMaterial({
+        color,
+        opacity: 0.5,
+        transparent: true,
+        depthWrite: false,
+      });
 
-    this.helperMesh = new THREE.Group();
-    this.helperMesh.add(new THREE.Mesh(cylinderGeometry, material));
-    this.helperMesh.add(new THREE.Mesh(toroidGeometry, material));
+      this.helperMesh = new THREE.Group();
+      this.helperMesh.add(new THREE.Mesh(cylinderGeometry, material));
+      this.helperMesh.add(new THREE.Mesh(toroidGeometry, material));
 
-    this.helperMesh.position.copy(mesh.position);
+      this.helperMesh.position.copy(mesh.position);
 
-    if (relative) {
-      this.helperMesh.setRotationFromEuler(mesh.rotation);
-    }
+      if (relative) {
+        this.helperMesh.setRotationFromEuler(mesh.rotation);
+      }
 
-    if (axis === "y") {
-      this.helperMesh.rotateZ(Math.PI / 2);
-    }
-    if (axis === "z") {
-      this.helperMesh.rotateY(-Math.PI / 2);
-      this.helperMesh.rotateX(Math.PI / 2);
+      if (axis === 'y') {
+        this.helperMesh.rotateZ(Math.PI / 2);
+      }
+      if (axis === 'z') {
+        this.helperMesh.rotateY(-Math.PI / 2);
+        this.helperMesh.rotateX(Math.PI / 2);
+      }
+    } else if (mesh instanceof THREE.Group) {
+      // debugger;
+      const groups: THREE.Group[] = mesh.children.map(m => {
+        const rotHelper: RotationHelper = new RotationHelper(m, axis, relative);
+        return rotHelper.mesh;
+      });
+
+      this.helperMesh = new THREE.Group();
+
+      this.helperMesh.children = groups.flatMap(group =>
+        group.children.map(child => {
+          child.position.copy(group.position);
+          child.setRotationFromEuler(group.rotation);
+          return child;
+        })
+      );
     }
   }
 
--- packages/lib3d/src/TranslationHelper.ts
+++ packages/lib3d/src/TranslationHelper.ts
@@ -13,79 +13,106 @@
  * Last modified  : 2018-12-16 19:55:28
  */
 
-import * as THREE from "three";
+import * as THREE from 'three';
 
 export default class TranslationHelper {
   private helperMesh: THREE.Group;
 
   constructor(mesh: THREE.Object3D, axis: string, relative: boolean) {
-    const boundingBoxDims: THREE.Vector3 = new THREE.Vector3();
-    new THREE.Box3().setFromObject(mesh).getSize(boundingBoxDims);
-    const radius: number = 0.3;
-    let color: number;
-    const separation: number = 3;
-    const length: number = 20;
-    const arrowLength: number = 5;
-    let offset: number;
-    let offsetArrow: number;
+    if (
+      mesh instanceof THREE.Mesh ||
+      (mesh.userData &&
+        mesh.userData.type &&
+        mesh.userData.type === 'RepetitionObject')
+    ) {
+      const boundingBoxDims: THREE.Vector3 = new THREE.Vector3();
+      new THREE.Box3().setFromObject(mesh).getSize(boundingBoxDims);
+      const radius: number = 0.3;
+      let color: number;
+      const separation: number = 3;
+      const length: number = 20;
+      const arrowLength: number = 5;
+      let offset: number;
+      let offsetArrow: number;
 
-    if (axis === "x") {
-      color = 0xff0000;
-      offset = boundingBoxDims.x / 2 + separation + length / 2;
-      offsetArrow =
-        boundingBoxDims.x / 2 + separation + arrowLength / 2 + length;
-    } else if (axis === "y") {
-      color = 0x00ff00;
-      offset = boundingBoxDims.y / 2 + separation + length / 2;
-      offsetArrow =
-        boundingBoxDims.y / 2 + separation + arrowLength / 2 + length;
-    } else {
-      color = 0x0000ff;
-      offset = boundingBoxDims.z / 2 + separation + length / 2;
-      offsetArrow =
-        boundingBoxDims.z / 2 + separation + arrowLength / 2 + length;
-    }
+      if (axis === 'x') {
+        color = 0xff0000;
+        offset = boundingBoxDims.x / 2 + separation + length / 2;
+        offsetArrow =
+          boundingBoxDims.x / 2 + separation + arrowLength / 2 + length;
+      } else if (axis === 'y') {
+        color = 0x00ff00;
+        offset = boundingBoxDims.y / 2 + separation + length / 2;
+        offsetArrow =
+          boundingBoxDims.y / 2 + separation + arrowLength / 2 + length;
+      } else {
+        color = 0x0000ff;
+        offset = boundingBoxDims.z / 2 + separation + length / 2;
+        offsetArrow =
+          boundingBoxDims.z / 2 + separation + arrowLength / 2 + length;
+      }
 
-    const cylinderGeometry: THREE.Geometry = new THREE.CylinderGeometry(
-      radius,
-      radius,
-      length
-    );
-    cylinderGeometry.rotateZ(Math.PI / 2);
-    cylinderGeometry.translate(offset, 0, 0);
+      const cylinderGeometry: THREE.Geometry = new THREE.CylinderGeometry(
+        radius,
+        radius,
+        length
+      );
+      cylinderGeometry.rotateZ(Math.PI / 2);
+      cylinderGeometry.translate(offset, 0, 0);
 
-    const arrowGeometry: THREE.Geometry = new THREE.CylinderGeometry(
-      2 * radius,
-      0,
-      5,
-      20
-    );
-    arrowGeometry.rotateZ(Math.PI / 2);
-    arrowGeometry.translate(offsetArrow, 0, 0);
+      const arrowGeometry: THREE.Geometry = new THREE.CylinderGeometry(
+        2 * radius,
+        0,
+        5,
+        20
+      );
+      arrowGeometry.rotateZ(Math.PI / 2);
+      arrowGeometry.translate(offsetArrow, 0, 0);
 
-    const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({
-      color,
-      opacity: 0.5,
-      transparent: true,
-      depthWrite: false
-    });
+      const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({
+        color,
+        opacity: 0.5,
+        transparent: true,
+        depthWrite: false,
+      });
 
-    this.helperMesh = new THREE.Group();
-    this.helperMesh.add(new THREE.Mesh(cylinderGeometry, material));
-    this.helperMesh.add(new THREE.Mesh(arrowGeometry, material));
+      this.helperMesh = new THREE.Group();
+      this.helperMesh.add(new THREE.Mesh(cylinderGeometry, material));
+      this.helperMesh.add(new THREE.Mesh(arrowGeometry, material));
 
-    this.helperMesh.position.copy(mesh.position);
+      this.helperMesh.position.copy(mesh.position);
 
-    if (relative) {
-      this.helperMesh.setRotationFromEuler(mesh.rotation);
-    }
+      if (relative) {
+        this.helperMesh.setRotationFromEuler(mesh.rotation);
+      }
 
-    if (axis === "y") {
-      this.helperMesh.rotateZ(Math.PI / 2);
-    }
-    if (axis === "z") {
-      this.helperMesh.rotateY(-Math.PI / 2);
-      this.helperMesh.rotateX(Math.PI / 2);
+      if (axis === 'y') {
+        this.helperMesh.rotateZ(Math.PI / 2);
+      }
+      if (axis === 'z') {
+        this.helperMesh.rotateY(-Math.PI / 2);
+        this.helperMesh.rotateX(Math.PI / 2);
+      }
+    } else if (mesh instanceof THREE.Group) {
+      // debugger;
+      const groups: THREE.Group[] = mesh.children.map(m => {
+        const transHelper: TranslationHelper = new TranslationHelper(
+          m,
+          axis,
+          relative
+        );
+        return transHelper.mesh;
+      });
+
+      this.helperMesh = new THREE.Group();
+
+      this.helperMesh.children = groups.flatMap(group =>
+        group.children.map(child => {
+          child.position.copy(group.position);
+          child.setRotationFromEuler(group.rotation);
+          return child;
+        })
+      );
     }
   }
 
