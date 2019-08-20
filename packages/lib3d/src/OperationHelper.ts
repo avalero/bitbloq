ae45e6f62578fc5108ca021f96d40cca889ec6ac
Alberto Valero <alberto.valero@bq.com>
2019-08-20 12:02:32 +0200

    Unified Translation and Rotation Helper

--- packages/lib3d/src/OperationHelper.ts
+++ packages/lib3d/src/OperationHelper.ts
@@ -1,16 +1,10 @@
-/**
- * Copyright (c) 2018 Bitbloq (BQ)
- *
- * License: MIT
- *
- * long description for the file
- *
- * @summary short description for the file
- * @author David García <https://github.com/empoalp>,
- * @author Alberto Valero <https://github.com/avalero>
- *
- * Created at     : 2018-10-17 12:30:31
- * Last modified  : 2018-12-16 19:55:28
+/*
+ * File: OperationHelper.ts
+ * Project: Bitbloq
+ * License: MIT (https://opensource.org/licenses/MIT)
+ * Bitbloq Repository: https://github.com/bitbloq
+ * Bitbloq Team: https://github.com/orgs/Bitbloq/people
+ * Copyright 2018 - 2019 BQ Educacion.
  */
 
 import * as THREE from 'three';
@@ -20,13 +14,31 @@ import RepetitionObject from './RepetitionObject';
 import CompoundObject from './CompoundObject';
 import ObjectsGroup from './ObjectsGroup';
 
-export default class TranslationHelper {
+enum HelperType {
+  Rotation = 'rotation',
+  Translation = 'translation',
+}
+
+enum HelperAxis {
+  X = 'x',
+  Y = 'y',
+  Z = 'z',
+}
+
+export default class OperationHelper {
   private helperMesh: THREE.Group;
   private obj: ObjectsCommon;
-  private axis: string;
+  private axis: HelperAxis;
   private relative: boolean;
+  private type: HelperType;
 
-  constructor(obj: ObjectsCommon, axis: string, relative: boolean) {
+  constructor(
+    obj: ObjectsCommon,
+    type: HelperType,
+    axis: HelperAxis,
+    relative: boolean
+  ) {
+    this.type = type;
     this.obj = obj;
     this.axis = axis;
     this.relative = relative;
@@ -42,8 +54,9 @@ export default class TranslationHelper {
 
     const parent = this.obj.getParent();
     if (parent instanceof RepetitionObject) {
-      this.helperMesh = await new TranslationHelper(
+      this.helperMesh = await new OperationHelper(
         parent,
+        this.type,
         this.axis,
         true
       ).getHelperMeshAsync();
@@ -51,8 +64,9 @@ export default class TranslationHelper {
     }
 
     if (parent instanceof ObjectsGroup) {
-      this.helperMesh = await new TranslationHelper(
+      this.helperMesh = await new OperationHelper(
         this.obj.userData.objectClone,
+        this.type,
         this.axis,
         this.relative
       ).getHelperMeshAsync();
@@ -60,7 +74,11 @@ export default class TranslationHelper {
     }
 
     const mesh = await this.obj.getMeshAsync();
-    this.helperMesh = this.buildHelper(mesh as THREE.Mesh);
+    this.helperMesh =
+      this.type === HelperType.Translation
+        ? this.buildTranslationHelper(mesh as THREE.Mesh)
+        : this.buildRotationHelper(mesh as THREE.Mesh);
+
     const positionCalculator = await new PositionCalculator(
       this.obj
     ).getPositionAsync();
@@ -111,10 +129,10 @@ export default class TranslationHelper {
     }
 
     // rotate according to the axis
-    if (this.axis === 'y') {
+    if (this.axis === HelperAxis.Y) {
       this.helperMesh.rotateZ(Math.PI / 2);
     }
-    if (this.axis === 'z') {
+    if (this.axis === HelperAxis.Z) {
       this.helperMesh.rotateY(-Math.PI / 2);
       this.helperMesh.rotateX(Math.PI / 2);
     }
@@ -128,8 +146,9 @@ export default class TranslationHelper {
 
     const childrenHelpers: THREE.Group[] = await Promise.all(
       children.map(child => {
-        const helper = new TranslationHelper(
+        const helper = new OperationHelper(
           child.userData.objectClone,
+          this.type,
           this.axis,
           this.relative
         );
@@ -141,7 +160,63 @@ export default class TranslationHelper {
     return this.helperMesh;
   }
 
-  private buildHelper(mesh: THREE.Mesh) {
+  private buildRotationHelper(mesh: THREE.Mesh): THREE.Group {
+    const boundingBoxDims: THREE.Vector3 = new THREE.Vector3();
+    new THREE.Box3().setFromObject(mesh).getSize(boundingBoxDims);
+    const radius: number = 0.5;
+    let color: number;
+    const separation: number = 10;
+    let length: number;
+    const extraLength: number = 30;
+    let toroidRadius: number;
+    const toroidArc: number = 2 * Math.PI;
+    const toroidInnerRadius: number = 0.7;
+
+    if (this.axis === HelperAxis.X) {
+      color = 0xff0000;
+      toroidRadius =
+        Math.max(boundingBoxDims.y, boundingBoxDims.z) / 2 + separation;
+      length = Math.max(boundingBoxDims.y, boundingBoxDims.z) + extraLength;
+    } else if (this.axis === HelperAxis.Y) {
+      color = 0x00ff00;
+      toroidRadius =
+        Math.max(boundingBoxDims.x, boundingBoxDims.z) / 2 + separation;
+      length = Math.max(boundingBoxDims.x, boundingBoxDims.z) + extraLength;
+    } else {
+      color = 0x0000ff;
+      toroidRadius =
+        Math.max(boundingBoxDims.x, boundingBoxDims.y) / 2 + separation;
+      length = Math.max(boundingBoxDims.x, boundingBoxDims.y) + extraLength;
+    }
+
+    const cylinderGeometry = new THREE.CylinderGeometry(radius, radius, length);
+    cylinderGeometry.rotateZ(Math.PI / 2);
+
+    const toroidGeometry = new THREE.TorusGeometry(
+      toroidRadius,
+      toroidInnerRadius,
+      6,
+      toroidRadius * 2, // lets make number of segments equal to radius * factor
+      toroidArc
+    );
+
+    toroidGeometry.rotateY(Math.PI / 2);
+
+    const material = new THREE.MeshBasicMaterial({
+      color,
+      opacity: 0.5,
+      transparent: true,
+      depthWrite: false,
+    });
+
+    this.helperMesh = new THREE.Group();
+    this.helperMesh.add(new THREE.Mesh(cylinderGeometry, material));
+    this.helperMesh.add(new THREE.Mesh(toroidGeometry, material));
+
+    return this.helperMesh;
+  }
+
+  private buildTranslationHelper(mesh: THREE.Mesh) {
     const boundingBoxDims: THREE.Vector3 = new THREE.Vector3();
     new THREE.Box3().setFromObject(mesh).getSize(boundingBoxDims);
     const radius: number = 0.3;
@@ -152,12 +227,12 @@ export default class TranslationHelper {
     let offset: number;
     let offsetArrow: number;
 
-    if (this.axis === 'x') {
+    if (this.axis === HelperAxis.X) {
       color = 0xff0000;
       offset = boundingBoxDims.x / 2 + separation + length / 2;
       offsetArrow =
         boundingBoxDims.x / 2 + separation + arrowLength / 2 + length;
-    } else if (this.axis === 'y') {
+    } else if (this.axis === HelperAxis.Y) {
       color = 0x00ff00;
       offset = boundingBoxDims.y / 2 + separation + length / 2;
       offsetArrow =
