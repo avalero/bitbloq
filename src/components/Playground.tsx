bf865e7fcb19fc541363913987b0dda9a59d21da
David Garcia Paredes <david.garciaparedes@bq.com>
2019-08-22 10:39:10 +0200

    Allow playground to save

--- src/components/Playground.tsx
+++ src/components/Playground.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
+import saveAs from "file-saver";
 import { documentTypes } from "../config";
 
 interface PlaygroundProps {
@@ -7,15 +8,37 @@ interface PlaygroundProps {
 
 const Playground: React.FunctionComponent<PlaygroundProps> = ({ type }) => {
   const [tabIndex, setTabIndex] = useState(0);
+  const contentRef = useRef([]);
   const documentType = documentTypes[type];
   const EditorComponent = documentType.editorComponent;
 
+  const onSaveDocument = () => {
+    const title = "escena";
+    const documentJSON = {
+      type,
+      title,
+      content: JSON.stringify(contentRef.current)
+    };
+    var blob = new Blob([JSON.stringify(documentJSON)], {
+      type: "text/json;charset=utf-8"
+    });
+    saveAs(blob, `${title}.${type}.bitbloq`);
+  };
+
+  const onContentChange = content => {
+    contentRef.current = content;
+  };
+
+
   return (
     <EditorComponent
+      content={contentRef.current}
+      onContentChange={onContentChange}
       brandColor={documentType.color}
       title="Playground"
       tabIndex={tabIndex}
       onTabChange={setTabIndex}
+      onSaveDocument={onSaveDocument}
     />
   );
 };
