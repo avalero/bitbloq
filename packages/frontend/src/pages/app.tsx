1794bd45600c860c1fb8fc9e882200e825173490
Carlos <carlosmoro95@gmail.com>
2019-10-04 13:19:58 +0200

    SWBIT-4209 #comment Doesn't check user when it activates the user #resolved #fixed

--- packages/frontend/src/pages/app.tsx
+++ packages/frontend/src/pages/app.tsx
@@ -74,10 +74,16 @@ const Route = ({
 
   return (
     <Suspense fallback={<Loading type={type} />}>
-      <UserDataProvider>
+      {rest.path === "/app/activate" ? (
         <Component {...rest} type={type} />
-      </UserDataProvider>
-      {requiresSession && <SessionWarningModal />}
+      ) : (
+        <>
+          <UserDataProvider>
+            <Component {...rest} type={type} />
+          </UserDataProvider>
+          {requiresSession && <SessionWarningModal />}
+        </>
+      )}
     </Suspense>
   );
 };
