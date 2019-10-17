3a869e4cc3f4992afbc278fe64de49e4a1c69c99
Carlos <carlosmoro95@gmail.com>
2019-10-17 15:29:36 +0200

    SWBIT-4217 Separates LoginPanel and LoginForm

--- packages/frontend/src/components/LoginPanel.tsx
+++ packages/frontend/src/components/LoginPanel.tsx
@@ -3,6 +3,54 @@ import styled from "@emotion/styled";
 import { Link } from "gatsby";
 import { Input, Button } from "@bitbloq/ui";
 
+interface FormProps {
+  className?: string;
+  email: string;
+  logingError: boolean;
+  password: string;
+  setEmail(email: string): void;
+  setPassword(password: string): void;
+}
+
+export const LoginForm: FC<FormProps> = ({
+  className,
+  email,
+  logingError,
+  password,
+  setEmail,
+  setPassword
+}) => {
+  return (
+    <div className={className}>
+      <FormGroup>
+        <label>Correo electrónico</label>
+        <Input
+          name="email"
+          type="text"
+          placeholder="Correo electrónico"
+          value={email}
+          error={logingError}
+          onChange={e => setEmail(e.target.value)}
+        />
+      </FormGroup>
+      <FormGroup>
+        <label>Contraseña</label>
+        <Input
+          name="email"
+          type="password"
+          placeholder="Contraseña"
+          value={password}
+          error={logingError}
+          onChange={e => setPassword(e.target.value)}
+        />
+      </FormGroup>
+      {logingError && (
+        <ErrorMessage>Correo electrónico o contraseña no válidos</ErrorMessage>
+      )}
+    </div>
+  );
+};
+
 interface LoginPanelProps {
   className?: string;
   email: string;
@@ -34,47 +82,20 @@ const LoginPanel: FC<LoginPanelProps> = (props: LoginPanelProps) => {
       className={className}
       onSubmit={(event: Event) => event.preventDefault()}
     >
-      <FormGroup>
-        <label>Correo electrónico</label>
-        <Input
-          name="email"
-          type="text"
-          placeholder="Correo electrónico"
-          value={email}
-          error={logingError}
-          onChange={e => setEmail(e.target.value)}
-        />
-      </FormGroup>
-      <FormGroup>
-        <label>Contraseña</label>
-        <Input
-          name="email"
-          type="password"
-          placeholder="Contraseña"
-          value={password}
-          error={logingError}
-          onChange={e => setPassword(e.target.value)}
-        />
-      </FormGroup>
-      {logingError && (
-        <ErrorMessage>Correo electrónico o contraseña no válidos</ErrorMessage>
-      )}
-      <Button
-        type="submit"
-        onClick={() => onLoginClick()}
-        disabled={logingIn}
-      >
+      <LoginForm
+        email={email}
+        logingError={logingError}
+        password={password}
+        setEmail={setEmail}
+        setPassword={setPassword}
+      />
+      <Button type="submit" onClick={() => onLoginClick()} disabled={logingIn}>
         Entrar
       </Button>
-      <Button
-        secondary
-        onClick={secondaryButtonCallback}
-      >
+      <Button secondary onClick={secondaryButtonCallback}>
         {secondaryButtonText}
       </Button>
-      <Link to="/forgot-password">
-        No recuerdo mi contraseña
-      </Link>
+      <Link to="/forgot-password">No recuerdo mi contraseña</Link>
     </Panel>
   );
 };
