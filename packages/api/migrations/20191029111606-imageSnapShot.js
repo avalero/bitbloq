15e080ff13dce6f97e0851e561d8f06968b9fdc5
aldaMarMu <alda.mar308@gmail.com>
2019-10-29 16:08:05 +0100

    mongodb migrations working

---
+++ packages/api/migrations/20191029111606-imageSnapShot.js
@@ -0,0 +1,31 @@
+module.exports = {
+  up(db) {
+    // TODO write your migration here. Return a Promise (and/or use async & await).
+    return db
+      .collection("documentmodels")
+      .find({})
+      .forEach(element => {
+        if (element.image && typeof(element.image) === "string") {
+          db.collection("documentmodels").updateOne(
+            { _id: element._id },
+            { $set: { image: { image: element.image, isSnapshot: element.image.indexOf('blob')>-1 } } }
+          );
+        }
+      });
+  },
+
+  down(db) {
+    // TODO write the statements to rollback your migration (if possible)
+    return db
+      .collection("documentmodels")
+      .find({})
+      .forEach(element => {
+        if (element.image.image) {
+          db.collection("documentmodels").updateOne(
+            { _id: element._id },
+            { $set: { image: element.image.image } }
+          );
+        }
+      });
+  }
+};
