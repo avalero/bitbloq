6f901f3dcd4e6a7096b950d1fb314f19385217b5
aldaMarMu <alda.mar308@gmail.com>
2019-10-29 13:32:43 +0100

    SWBIT-4341 fixed error in examples images

--- packages/frontend/src/components/LandingExamples.tsx
+++ packages/frontend/src/components/LandingExamples.tsx
@@ -37,6 +37,7 @@ const LandingExamples: FC = () => {
         <ExamplesWrap>
           <Examples style={examplesStyle}>
             {data.examples.map((example: any) => (
+              example.image=example.image.image,
               <Example key={example.id} onClick={() => onExampleClick(example)}>
                 <DndProvider backend={HTML5Backend}>
                   <DocumentCard draggable={false} document={example} />
