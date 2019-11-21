ee32e10a0686d64dfcb178796648c077c44f49a1
Carmen <carmen.dominguez@bq.com>
2019-11-21 17:20:46 +0100

    Fix signup redirect

--- packages/frontend/src/pages/signup.tsx
+++ packages/frontend/src/pages/signup.tsx
@@ -1,17 +1,12 @@
-import { NextPage } from "next";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
-import withApollo from "../apollo/withApollo";
-import Loading from "../components/Loading";
+import redirect from "../lib/redirect";
 
-const SignupPage: NextPage = () => {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push("/signup/user-data");
-  });
-
-  return <Loading />;
+const SignupPage = () => {
+  return null;
 };
 
-export default withApollo(SignupPage, { requiresSession: false });
+SignupPage.getInitialProps = async ctx => {
+  redirect(ctx, "/signup/user-data");
+  return {};
+};
+
+export default SignupPage;
