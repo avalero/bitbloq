70985e721fc2034cbabbda5d5ac996ff1f123eaa
David Garcia <empoalp@gmail.com>
2019-11-21 13:41:24 +0100

    Fixes for session

--- packages/frontend/src/config.js
+++ packages/frontend/src/config.js
@@ -185,7 +185,7 @@ export const featureTable = [
 
 const defaultFlags = {
   RENEW_TOKEN_SECONDS: 60,
-  TOKEN_DURATION_MINUTES: 300,
+  TOKEN_DURATION_MINUTES: 60,
   TOKEN_WARNING_SECONDS: 300,
   SHOW_GRAPHQL_LOGS: false
 };
