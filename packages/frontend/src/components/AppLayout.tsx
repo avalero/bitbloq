baac08687eae830cae00694dfd9e3a05193efeae
Carlos <carlos.moro@bq.com>
2019-12-10 11:42:14 +0100

    SWBIT-4400 #comment Fixes double AppLayout

--- packages/frontend/src/components/AppLayout.tsx
+++ packages/frontend/src/components/AppLayout.tsx
@@ -6,13 +6,19 @@ import AppHeader from "./AppHeader";
 import UserSession from "./UserSession";
 
 interface IAppLayoutProps {
+  className?: string;
   header?: JSX.Element | string;
   loading?: boolean;
 }
 
-const AppLayout: FC<IAppLayoutProps> = ({ header, loading, children }) => {
+const AppLayout: FC<IAppLayoutProps> = ({
+  className,
+  header,
+  loading,
+  children
+}) => {
   return (
-    <Container>
+    <Container className={className}>
       <AppHeader>
         <UserSession />
       </AppHeader>
