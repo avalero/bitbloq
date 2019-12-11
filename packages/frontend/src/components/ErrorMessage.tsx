1c682e8a6eeb25f54cd9c5091a84a33b3cbf292c
Carlos <carlos.moro@bq.com>
2019-12-11 15:46:27 +0100

    SWBIT-4519 #comment Uses colors in ErrorMessage component

--- packages/frontend/src/components/ErrorMessage.tsx
+++ packages/frontend/src/components/ErrorMessage.tsx
@@ -1,9 +1,10 @@
+import { colors } from "@bitbloq/ui";
 import styled from "@emotion/styled";
 
 const ErrorMessage = styled.div`
   font-size: 12px;
   font-style: italic;
-  color: #d82b32;
+  color: ${colors.red};
 `;
 
 export default ErrorMessage;
