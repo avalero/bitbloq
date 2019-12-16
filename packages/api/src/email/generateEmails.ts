66765d12141b2df37d93829584e22fbd5fa89a2a
aldaMarMu <alda.mar308@gmail.com>
2019-12-16 09:17:18 +0100

    SWBIT-4574 simplified code

--- packages/api/src/email/generateEmails.ts
+++ packages/api/src/email/generateEmails.ts
@@ -4,11 +4,7 @@ import * as path from "path";
 import mjml2html from "mjml";
 import { ApolloError } from "apollo-server";
 
-export const generateChangeEmailEmail = async (params): Promise<string> => {
-  const result = fs.readFileSync(
-    path.join(__dirname, "./changeEmail.mjml"),
-    "utf8"
-  );
+const generateHTML = (result, params) => {
   if (result) {
     const template = compile(result);
     const mail = mjml2html(template(params), {
@@ -20,6 +16,14 @@ export const generateChangeEmailEmail = async (params): Promise<string> => {
   } else {
     throw new ApolloError("Error generating email", "ERROR_GENERATING_MAIL");
   }
+};
+
+export const generateChangeEmailEmail = async (params): Promise<string> => {
+  const result = fs.readFileSync(
+    path.join(__dirname, "./changeEmail.mjml"),
+    "utf8"
+  );
+  return generateHTML(result, params);
 };
 
 export const generateResetPasswordEmail = async (params): Promise<string> => {
@@ -27,17 +31,7 @@ export const generateResetPasswordEmail = async (params): Promise<string> => {
     path.join(__dirname, "./resetPassword.mjml"),
     "utf8"
   );
-  if (result) {
-    const template = compile(result);
-    const mail = mjml2html(template(params), {
-      keepComments: false,
-      beautify: true,
-      minify: true
-    });
-    return mail.html;
-  } else {
-    throw new ApolloError("Error generating email", "ERROR_GENERATING_MAIL");
-  }
+  return generateHTML(result, params);
 };
 
 export const generateWelcomeEmail = async (params): Promise<string> => {
@@ -45,15 +39,5 @@ export const generateWelcomeEmail = async (params): Promise<string> => {
     path.join(__dirname, "./welcome.mjml"),
     "utf8"
   );
-  if (result) {
-    const template = compile(result);
-    const mail = mjml2html(template(params), {
-      keepComments: false,
-      beautify: true,
-      minify: true
-    });
-    return mail.html;
-  } else {
-    throw new ApolloError("Error generating email", "ERROR_GENERATING_MAIL");
-  }
+  return generateHTML(result, params);
 };
