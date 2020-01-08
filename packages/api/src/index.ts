0572d670c8306dffd65ec106f2558927baebbdae
aldaMarMu <alda.mar308@gmail.com>
2020-01-08 13:25:01 +0100

    fixed errors with api package

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,17 +1,2 @@
-import * as Types from "./api-types";
-import * as ConfigParameters from "./config";
-
-export type IDocImageIn = Types.IDocImageIn;
-export type IDocument = Types.IDocument;
-export type IExercise = Types.IExercise;
-export type IFolder = Types.IFolder;
-export type IMutationLoginArgs = Types.IMutationLoginArgs;
-export type IResource = Types.IResource;
-export type IResult = Types.IResult;
-export type ISessionExpires = Types.ISessionExpires;
-export type ISocialLogin = Types.ISocialLogin;
-export type IUser = Types.IUser;
-export type IUserIn = Types.IUserIn;
-export type IUserStep1 = Types.IUserStep1;
-
-export const LIMIT_SIZE = ConfigParameters.LIMIT_SIZE;
+export * from "./api-types";
+export * from "./config";
