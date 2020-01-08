144185c72ae0a5eafe6e20bd18386f4868db3ef0
Carlos <carlos.moro@bq.com>
2020-01-08 15:59:56 +0100

    SWBIT-4625 Limits the rate range

--- packages/frontend/src/components/ErrorMessage.tsx
+++ packages/frontend/src/components/ErrorMessage.tsx
@@ -1,10 +1,11 @@
 import { colors } from "@bitbloq/ui";
 import styled from "@emotion/styled";
 
-const ErrorMessage = styled.div`
+const ErrorMessage = styled.div<{ hide?: boolean }>`
+  color: ${colors.red};
+  display: ${props => (props.hide ? "none" : "block")};
   font-size: 12px;
   font-style: italic;
-  color: ${colors.red};
   margin-top: 10px;
 `;
 
--- packages/frontend/src/types.ts
+++ packages/frontend/src/types.ts
@@ -25,13 +25,6 @@ export interface IEditorProps {
   children: (documentProps: Partial<IDocumentProps>) => JSX.Element;
 }
 
-export interface ISubmission extends IDocument {
-  exercise: string;
-  grade: number;
-  studentNick: string;
-  teacherComment: string;
-}
-
 export interface IPlan {
   name: string;
   bitbloqCloud?: boolean;
