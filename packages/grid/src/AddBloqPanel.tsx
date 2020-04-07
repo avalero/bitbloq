c7018008290b0a42341813badf86b28e47ef6aa0
Carmen <carmen.dominguez@bq.com>
2020-04-07 11:27:04 +0200

    BMJUN-540 Add bloq information

--- packages/grid/src/AddBloqPanel.tsx
+++ packages/grid/src/AddBloqPanel.tsx
@@ -43,11 +43,20 @@ const AddBloqPanel: FC<IAddBloqPanelProps> = ({
           {Object.keys(availableBloqs).map(typeName => {
             const type = bloqTypes.find(t => t.name === typeName)!;
             return (
-              <StyledBloq
-                key={type.name}
-                type={type}
-                onClick={() => onSelectBloqType(type)}
-              />
+              <Bloq>
+                <HorizontalBloq
+                  key={type.name}
+                  type={type}
+                  onClick={() => onSelectBloqType(type)}
+                />
+                <BloqInformation>
+                  {availableBloqs[typeName] > 0 ? (
+                    availableBloqs[typeName]
+                  ) : (
+                    <>&#8734;</>
+                  )}
+                </BloqInformation>
+              </Bloq>
             );
           })}
         </Bloqs>
@@ -59,6 +68,45 @@ const AddBloqPanel: FC<IAddBloqPanelProps> = ({
 export default AddBloqPanel;
 
 /* Styled components */
+
+const Bloq = styled.div`
+  &:not(:last-of-type) {
+    margin-right: 10px;
+  }
+`;
+
+const BloqInformation = styled.div`
+  font-size: 20px;
+  font-weight: bold;
+  height: 24px;
+  padding-top: 5px;
+  text-align: center;
+`;
+
+interface IBloqPlaceholderWrapProps {
+  left: number;
+}
+
+const BloqPlaceholderWrap = styled.div<IBloqPlaceholderWrapProps>`
+  position: absolute;
+  bottom: 0px;
+  transform: translate(0, 100%);
+  left: ${props => props.left}px;
+  background-color: white;
+  padding: 10px;
+  border-bottom-left-radius: 6px;
+  border-bottom-right-radius: 6px;
+`;
+
+const Bloqs = styled.div`
+  display: flex;
+`;
+
+const BloqsWrapper = styled.div`
+  display: flex;
+  overflow: auto;
+  width: 100%;
+`;
 
 const Container = styled.div`
   align-items: center;
@@ -93,33 +141,4 @@ const CloseButton = styled.div`
     height: 20px;
     width: 20px;
   }
-`;
-
-interface IBloqPlaceholderWrapProps {
-  left: number;
-}
-
-const BloqPlaceholderWrap = styled.div<IBloqPlaceholderWrapProps>`
-  position: absolute;
-  bottom: 0px;
-  transform: translate(0, 100%);
-  left: ${props => props.left}px;
-  background-color: white;
-  padding: 10px;
-  border-bottom-left-radius: 6px;
-  border-bottom-right-radius: 6px;
-`;
-
-const Bloqs = styled.div`
-  display: flex;
-`;
-
-const BloqsWrapper = styled.div`
-  display: flex;
-  overflow: auto;
-  width: 100%;
-`;
-
-const StyledBloq = styled(HorizontalBloq)`
-  margin: 7px;
 `;
